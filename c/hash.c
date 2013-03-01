03bf60980e9cccc10d43e2e47adf1fbbf1d75128
Dan Cleary <dcc635@gmail.com>
2013-03-01 05:27:19 +0000

    Got insertion/search basically working

--- c/hash.c
+++ c/hash.c
@@ -1,10 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-#define TABLE_LENGTH 100
+
+#define TABLE_LENGTH 20
+#define MAX_STR_LEN 50
 
 struct hashnode{
-    char *key;
+    char key[MAX_STR_LEN];
     int value;
     struct hashnode *next;
     struct hashnode *prev;
@@ -16,30 +19,47 @@ struct hashtable{
 };
 typedef struct hashtable Hashtable;
 
+void hash_init(Hashtable *myhash){
+    int i;
+    Hashnode mynode = { "", -1, NULL, NULL };
+    for (i = 0; i < TABLE_LENGTH; i++){
+        myhash->table[i] = mynode;
+    }
+}
+
 int hash_function(char *key){
     int i = 0;
     int sum = 0;
-    while (NULL != key[i]){
+    while ('\0' != key[i]){
         sum += key[i];
         i++; 
     }
     return sum % TABLE_LENGTH;
 }
 
-void hash_insert(Hashtable **myhash, char *key, int value){
-    i = hash_function(key);
-    myhash->table[i] = value;
+void hash_insert(Hashtable *myhash, char *key, int value){
+    int i = hash_function(key);
+    Hashnode newnode;
+    strcpy(newnode.key, key);
+    newnode.value = value;
+    myhash->table[i] = newnode;
 }
     
+int hash_search(Hashtable *myhash, char *key){
+    int i = hash_function(key);
+    return (myhash->table[i]).value;
+}
 
 int main(void){
     Hashtable myhash;
+    hash_init(&myhash);
     hash_insert(&myhash, "tim", 27);
     hash_insert(&myhash, "james", 24);
     hash_insert(&myhash, "dan", 23);
-    printf("Dan's age: %d", hash_search(&myhash, "dan"));
-    printf("Tim's age: %d", hash_search(&myhash, "tim"));
-    printf("James' age: %d", hash_search(&myhash, "james"));
-    hash_delete(&myhash, "james");
-    printf("James' age: %d", hash_search(&myhash, "james"));
+//    printf("Dan's age: %d", hash_search(&myhash, "dan"));
+    printf("Tim's age: %d\n", hash_search(&myhash, "tim"));
+    printf("Dan's age: %d\n", hash_search(&myhash, "dan"));
+    printf("James' age: %d\n", hash_search(&myhash, "james"));
+//    hash_delete(&myhash, "james");
+//    printf("James' age: %d", hash_search(&myhash, "james"));
 }
